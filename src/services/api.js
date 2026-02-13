fe53b9e425824251dcce5d0ec54db0236a075b4e
sivanika <sivanika2025@gmail.com>
2026-02-13 21:44:40 +0530

    Update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://tutor-backend-jcxh.onrender.com/api",
 });
 
 // Attach JWT token to every request
