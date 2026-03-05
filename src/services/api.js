d6aae9ceba001595ca396b26a75f07cbee81aadb
sivanika <sivanika2025@gmail.com>
2026-03-05 12:48:57 +0530

    api update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://tutor-backend-vjpj.onrender.com/api",
 });
 
 
